423112bb3498ca089ba7fd1bfa7067a91c353e4f
Luis Figueira <la.figueira+ssw@gmail.com>
2011-01-31 18:10:55 +0000

    labels correctly named institution field is now mandatory and validated.

--- app/models/ssamr_user_detail.rb
+++ app/models/ssamr_user_detail.rb
@@ -2,4 +2,13 @@ class SsamrUserDetail < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of :description
+
+  validate :check_institution
+
+  def check_institution()
+    errors.add(:institution_id, "Please insert an institution") if
+      institution_id.blank? and other_institution.blank?
+  end
+
+
 end
