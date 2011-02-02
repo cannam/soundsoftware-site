760ad553bcf7a6456982da0debb7742e0e10ee14
Chris Cannam <chris.cannam@soundsoftware.ac.uk>
2011-02-02 17:04:56 +0000

    * Another minor stylesheet update, start hacking around with Doxyfiles

--- extra/soundsoftware/extract-javadoc.sh
+++ extra/soundsoftware/extract-javadoc.sh
@@ -18,18 +18,44 @@ fi
 
 # TODO: Doxyfile
 
+doxyfile=`find "$hgdir" -type f -name Doxyfile -print | head -1`
+
+echo "This project contains a Doxyfile:"
+echo "$doxyfile"
+
+if [ -z "$doxyfile" ]; then
+    echo "No Doxyfile: skipping"
+else
+
+# hmm. should be a whitelist
+
+    ( cd "$hgdir" && grep -vi OUTPUT_DIRECTORY "$doxyfile" | grep -vi HTML_OUTPUT | sed -e '$a OUTPUT_DIRECTORY='"$docdir" -e '$a HTML_OUTPUT = .' | doxygen - )
+
+fi
 
 # This is very rough; check what is actually permitted for package
 # declarations
 
 java_packages=`find "$hgdir" -type f -name \*.java -print | \
-     xargs grep -h '^ *package [a-zA-Z][a-zA-Z0-9\._-]* *; *' | sort | uniq | \
-     sed -e 's/^ *package //' -e 's/ *; *$//'`
+    xargs grep -h '^ *package [a-zA-Z][a-zA-Z0-9\._-]* *; *' | \
+    sort | uniq | \
+    sed -e 's/^ *package //' -e 's/ *; *$//'`
 
-echo "This project contains java packages:"
+echo "This project contains Java packages:"
 echo "$java_packages"
 
-# This won't work if code is in a subdir, e.g. src/com/example/project/Hello.java
+if [ -z "$java_packages" ]; then
+    echo "No Java packages: skipping"
+    exit 0
+fi
+
+# This won't work if code is in a subdir,
+# e.g. src/com/example/project/Hello.java
+
+# We need to convert the package name back to a path, and check
+# whether that matches the tail of the path to a java file that
+# declares itself to be in that package... but we don't have that list
+# of java files to hand here... hm
 
 javadoc -sourcepath "$hgdir" -d . -subpackages $java_packages -verbose
 
