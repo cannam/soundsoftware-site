0696fc413027d0250ecbcd826b0c9eaee50e3703
luisf <luis.figueira@eecs.qmul.ac.uk>
2011-03-24 15:34:01 +0000

    created the plugin

---
+++ vendor/plugins/redmine_bibliography/init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_bibliography do
+  name 'Redmine Bibliography plugin'
+  author 'Chris Cannam, Luis Figueira'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
