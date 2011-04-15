f0e0734c016c23135abb9fc8fc9d7995b47afbd8
luisf <luis.figueira@eecs.qmul.ac.uk>
2011-04-15 16:58:37 +0100

    added a new column to the publications table: reviewed. Attention: need to rebuild the db schema for the plugin.

--- vendor/plugins/redmine_bibliography/db/migrate/002_create_publications.rb
+++ vendor/plugins/redmine_bibliography/db/migrate/002_create_publications.rb
@@ -2,6 +2,7 @@ class CreatePublications < ActiveRecord::Migration
   def self.up
     create_table :publications do |t|
       t.column :title, :string
+      t.column :reviewed, :boolean, :default => false
     end
   end
 
