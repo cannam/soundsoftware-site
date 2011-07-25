69e5bc2c18ba27d1d7adaf5c7fe02292b44a9031
luisf <luis.figueira@eecs.qmul.ac.uk>
2011-07-25 16:33:06 +0100

    creates the association between authorship/author/user when saving a publication.

--- vendor/plugins/redmine_bibliography/app/models/authorship.rb
+++ vendor/plugins/redmine_bibliography/app/models/authorship.rb
@@ -5,13 +5,23 @@ class Authorship < ActiveRecord::Base
   accepts_nested_attributes_for :author
   accepts_nested_attributes_for :publication
   
+  
   # setter and getter for virtual attribute :user_id
   def user_id    
   end 
   
-  def user_id=(uid)
-    if User.find(uid).author.nil?      
-      User.find(uid).author = Author.new :name => User.find(uid).name
+  def user_id=(uid)  
+    unless uid.blank?
+      user = User.find(uid)                         
+      if user.author.nil?      
+        # TODO: should reflect the name_on_paper parameter
+        author = Author.new :name => user.name
+        author.save!
+        user.author = author
+        user.save!
+     end
+           
+    self.author_id = user.author.id
     end    
   end
 end
