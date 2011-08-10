31f65ba4fac2151563a0fbf928cf865c1442475c
Chris Cannam <chris.cannam@soundsoftware.ac.uk>
2011-08-10 14:24:53 +0100

    Layout changes for publication edit page.  Still requires correct authorships number in heading, and for the new authorship to be added in its proper place.

--- vendor/plugins/redmine_bibliography/app/helpers/publications_helper.rb
+++ vendor/plugins/redmine_bibliography/app/helpers/publications_helper.rb
@@ -51,7 +51,7 @@ module PublicationsHelper
   end
 
   def link_to_remove_fields(name, f)
-    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)")
+    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)", :class => 'icon icon-del')
   end
     
   def link_to_add_fields(name, f, association)
