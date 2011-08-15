a58d79e6e88bc588dd1ff3c11353242d804930d3
Chris Cannam <chris.cannam@soundsoftware.ac.uk>
2011-08-15 15:13:37 +0100

    Import redmine_tags plugin (from https://github.com/ixti/redmine_tags.git commit f09ad441f5dc39104b06) for testing

---
+++ vendor/plugins/redmine_tags/lib/redmine_tags/patches/auto_completes_controller_patch.rb
@@ -0,0 +1,38 @@
+# This file is a part of redmine_tags
+# redMine plugin, that adds tagging support.
+#
+# Copyright (c) 2010 Aleksey V Zapparov AKA ixti
+#
+# redmine_tags is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# redmine_tags is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with redmine_tags.  If not, see <http://www.gnu.org/licenses/>.
+
+require_dependency 'auto_completes_controller'
+
+module RedmineTags
+  module Patches
+    module AutoCompletesControllerPatch
+      def self.included(base)
+        base.send(:include, InstanceMethods)
+      end
+
+
+      module InstanceMethods
+        def issue_tags
+          @name = params[:q].to_s
+          @tags = Issue.available_tags :project_id => @project, :name_like => @name
+          render :layout => false, :partial => 'tag_list'
+        end
+      end
+    end
+  end
+end
