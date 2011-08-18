ce55eed728453f6f6dde4de7e6ccea6ba049642a
luisf <luis.figueira@eecs.qmul.ac.uk>
2011-08-18 13:02:03 +0100

    Created JS functions to simulate author being saved. Not finished.

--- vendor/plugins/redmine_bibliography/assets/javascripts/authors.js
+++ vendor/plugins/redmine_bibliography/assets/javascripts/authors.js
@@ -40,3 +40,26 @@ function identify_author_status(status, object_id) {
 		break;
 	}
 }
+
+function toggle_div(div_id){	
+	Effect.toggle(div_id, "appear", {duration:0.3});
+}
+
+function toggle_input_field(field_id){
+	$(field_id).addClassName('readonly');
+	$(field_id).next('em').hide();
+}
+
+function toggle_save_author(form_object_id){
+	toggle_input_field("publication_authorships_attributes_" + form_object_id + "_name_on_paper");
+	toggle_input_field("publication_authorships_attributes_" + form_object_id + "_institution");
+	toggle_input_field("publication_authorships_attributes_" + form_object_id + "_email");
+
+	toggle_div("publication_authorships_attributes_" + form_object_id +"_search_author");
+}
+
+function edit_author(form_object_id){
+	
+	
+}
+
