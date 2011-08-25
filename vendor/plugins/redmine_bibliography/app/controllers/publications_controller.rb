766fb5df70561bf794da9b347afe6d3ab25348f5
luisf <luis.figueira@eecs.qmul.ac.uk>
2011-08-25 15:38:51 +0100

    Addresses (completes?) Feature #242. Addresses Feature #241.

--- vendor/plugins/redmine_bibliography/app/controllers/publications_controller.rb
+++ vendor/plugins/redmine_bibliography/app/controllers/publications_controller.rb
@@ -6,10 +6,7 @@ class PublicationsController < ApplicationController
   
   model_object Publication
   before_filter :find_model_object, :except => [:new, :create, :index, :autocomplete_for_project, :add_author, :sort_author_order, :autocomplete_for_author, :get_user_info ]  
-
-  before_filter :find_project_by_project_id, :authorize, :only => :edit
-    
-    
+  before_filter :find_project_by_project_id, :authorize, :only => [ :edit, :new, :update, :create ]
     
   def new
     find_project_by_project_id
