855e42747a34d54e8fcdd40015f73e2f2d2b00dc
luisf <luis.figueira@eecs.qmul.ac.uk>
2013-01-09 11:53:24 +0000

    Fixed RedmineTags routes file (new Rails version standard).

--- plugins/redmine_tags/config/routes.rb
+++ plugins/redmine_tags/config/routes.rb
@@ -1,3 +1,3 @@
 RedmineApp::Application.routes.draw do
-  map.connect 'projects/set_fieldset_status', :controller => 'projects', :action => 'set_fieldset_status', :conditions => {:method => :post}
+  match 'projects/set_fieldset_status' => 'projects#set_fieldset_status', :constraints => {:method => :post}
 end
