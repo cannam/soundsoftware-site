d8e0e312853575beca33dcfae4e3570088f2f830
luisf <luis.figueira@eecs.qmul.ac.uk>
2013-03-26 15:11:27 +0000

    Added missing route to auto_completes#project_tags (AutoCompletes controller patched in redmine tags plugin)

--- plugins/redmine_tags/config/routes.rb
+++ plugins/redmine_tags/config/routes.rb
@@ -1,5 +1,6 @@
 RedmineApp::Application.routes.draw do
   match '/issue_tags/auto_complete/:project_id', :to => 'auto_completes#issue_tags', :via => :get, :as => 'auto_complete_issue_tags'
   match '/auto_completes/project_search_tags', :to => 'auto_completes#project_search_tags'
+  match '/auto_completes/project_tags', :to => 'auto_completes#project_tags'
   match 'projects/set_fieldset_status' => 'projects#set_fieldset_status', :constraints => {:method => :post}
 end
