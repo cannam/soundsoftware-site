94f201d34ae5f49ec77d131ada6920dc5b293bc4
luisf <luis.figueira@eecs.qmul.ac.uk>
2013-04-09 17:06:21 +0100

    Changed institutions javascript to jquery.

--- public/javascripts/ssamr_institutions.js
+++ public/javascripts/ssamr_institutions.js
@@ -3,27 +3,26 @@
 /* SSAMR specific functions */
 
 /* institution related functions */
-Event.observe(window, 'load',
-  function() {
-	$('ssamr_user_details_institution_type_true').observe('click', function(e, el) {
-	    $('ssamr_user_details_other_institution').disable();
-		$('ssamr_user_details_institution_id').enable();
+$(document).ready(function(){
+	$('#ssamr_user_details_institution_type_true').click(function() {
+        $('#ssamr_user_details_other_institution').attr('disabled', 'disabled');
+        $('#ssamr_user_details_institution_id').removeAttr('disabled');
 	});
 
-	$('ssamr_user_details_institution_type_false').observe('click', function(e, el) {
-	    $('ssamr_user_details_other_institution').enable();
-		$('ssamr_user_details_institution_id').disable();
+	$('#ssamr_user_details_institution_type_false').click(function() {
+        $('#ssamr_user_details_other_institution').removeAttr('disabled');
+        $('#ssamr_user_details_institution_id').attr('disabled', 'disabled');
 	});
-        
-    if($('ssamr_user_details_institution_type_true').checked)
-        $('ssamr_user_details_other_institution').disable();
+
+    if($('#ssamr_user_details_institution_type_true').checked)
+        $('#ssamr_user_details_other_institution').attr('disabled', 'disabled');
     else if($('ssamr_user_details_institution_type_false').checked)
-        $('ssamr_user_details_institution_id').disable();
+        $('#ssamr_user_details_institution_id').attr('disabled', 'disabled');
 	else {
-		$('ssamr_user_details_other_institution').disable();
-	  	$('ssamr_user_details_institution_id').enable();
-	  	$('ssamr_user_details_institution_type_true').checked = true;
-	  	$('ssamr_user_details_institution_type_false').checked = false;
+		$('#ssamr_user_details_other_institution').attr('disabled', 'disabled');
+        $('#ssamr_user_details_institution_id').removeAttr('disabled');
+        $('#ssamr_user_details_institution_type_true').checked = true;
+        $('#ssamr_user_details_institution_type_false').checked = false;
 	}
 }
 );
