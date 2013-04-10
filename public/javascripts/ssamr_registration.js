31c75617e7270fcde19ae170579d6a895208ea8c
luisf <luis.figueira@eecs.qmul.ac.uk>
2013-04-10 11:57:46 +0100

    Simplified JS on registration page.

--- public/javascripts/ssamr_registration.js
+++ public/javascripts/ssamr_registration.js
@@ -1,17 +1,6 @@
-
-
 /* SSAMR specific functions */
 
-/* institution related functions */
+/* initialisation: disabling  the other institution text box */
 $(document).ready(function(){
-		if(!$('#ssamr_user_details_institution_type_true').checked && $('#ssamr_user_details_institution_type_true').checked){
-            $('#ssamr_user_details_other_institution').attr('disabled', 'disabled');
-            $('#ssamr_user_details_institution_id').removeAttr('disabled');
-            $('#ssamr_user_details_institution_type_true').checked = true;
-            $('#ssamr_user_details_institution_type_false').checked = false;
-		}
-	}
-);
-
-
-
+        $('#ssamr_user_details_other_institution').attr('disabled', 'disabled');
+    });
