a4a90fdb097b1d0e00756b4941630d33e6058263
luisf <luis.figueira@eecs.qmul.ac.uk>
2013-05-10 17:50:56 +0100

    added missing route for autocomplete.

--- plugins/redmine_bibliography/config/routes.rb
+++ plugins/redmine_bibliography/config/routes.rb
@@ -2,6 +2,6 @@ RedmineApp::Application.routes.draw do
   resources :publications
 
   match "publications/show_bibtex_fields", :to => 'publications#show_bibtex_fields'
-
+  match "publications/autocomplete_for_author", :to => 'publications#autocomplete_for_author'
 
 end
