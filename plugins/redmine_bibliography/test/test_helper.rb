2a3cb5fa68b45391ad6a4892a59ca7fbc2c14f14
luisf <luis.figueira@eecs.qmul.ac.uk>
2013-08-21 17:51:08 +0100

    Correctly loading fixtures.

--- plugins/redmine_bibliography/test/test_helper.rb
+++ plugins/redmine_bibliography/test/test_helper.rb
@@ -4,24 +4,24 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
 require File.expand_path(File.dirname(__FILE__) + '/../app/controllers/publications_controller')
 
-# Ensure that we are using the temporary fixture path
-# Engines::Testing.set_fixture_path
-
 class BibliographyControllerTest < ActionController::TestCase
-  # fixtures :all
+  self.fixture_path = File.dirname(__FILE__) + "/fixtures/"
+
+  fixtures :authors
 
   def setup
+
   end
 
   def test_publication
     assert 1 == 0
   end
 
-  def test_routing
-    assert_routing(
-          {:method => :get, :path => '/requirements'},
-          :controller => 'requirements', :action => 'index'
-        )
-  end
+  # def test_routing
+  #   assert_routing(
+  #         {:method => :get, :path => '/requirements'},
+  #         :controller => 'requirements', :action => 'index'
+  #       )
+  # end
 
 end
