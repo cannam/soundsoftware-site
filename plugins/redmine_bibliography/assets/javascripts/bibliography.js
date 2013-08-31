08b959bfb8fb1826e8fb149a07f595aa92c3b52c
luisf <luis.figueira@eecs.qmul.ac.uk>
2013-08-31 18:36:57 +0100

    Authorship Edit/Save buttons now working properly in the publication#new view.

--- plugins/redmine_bibliography/assets/javascripts/bibliography.js
+++ plugins/redmine_bibliography/assets/javascripts/bibliography.js
@@ -5,6 +5,23 @@ function disable_fields(){
 	$author_info = $this.closest('div').prev();
 	$author_info.children('.description').toggle();
 	$author_info.find('p :input').attr("readonly", true);
+    $author_info.find('p :input').addClass('readonly');
+
+    $this.siblings('.author_edit_btn').show();
+    $this.hide();
+
+    return false;
+}
+
+function enable_fields(){
+    $this = $(this);
+    $author_info = $this.closest('div').prev();
+    $author_info.children('.description').toggle();
+    $author_info.find('p :input').attr("readonly", false);
+    $author_info.find('p :input').removeClass('readonly');
+
+    $this.siblings('.author_save_btn').show();
+    $this.hide();
 
     return false;
 }
