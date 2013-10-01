de9de106f5d8e7a325cd07e817e903982e02177d
luisf <luis.figueira@eecs.qmul.ac.uk>
2013-10-01 12:08:09 +0100

    Fixed the initial state of the Save/Edit buttons.

--- plugins/redmine_bibliography/assets/javascripts/new_publication.js
+++ plugins/redmine_bibliography/assets/javascripts/new_publication.js
@@ -4,4 +4,6 @@ $(document).ready(function(){
     // adds the events to the edit/save authorship button
     $('.author_save_btn').live('click', disable_fields);
     $('.author_edit_btn').live('click', enable_fields);
+
+
 });
