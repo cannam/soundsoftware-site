420860495b6b740ff34db29d0f5dc2f613029927
luisf <luis.figueira@eecs.qmul.ac.uk>
2013-11-07 17:09:55 +0000

    Extracting also top-level project stats in script.

--- extra/soundsoftware/get-statistics.rb
+++ extra/soundsoftware/get-statistics.rb
@@ -6,7 +6,12 @@
 # ./script/rails runner -e production extra/soundsoftware/get-statistics.rb
 #
 
-projectStats =  {:all => Project.active.all.count, :private => Project.active.find(:all, :conditions => {:is_public => false}).count}
+projectStats =  {
+        :all => Project.active.all.count,
+        :private => Project.active.find(:all, :conditions => {:is_public => false}).count,
+        :top_level => Project.active.find(:all, :conditions => {:parent_id => nil}).count,
+        :top_level_and_private => Project.active.find(:all, :conditions => {:is_public => false, :parent_id => nil}).count
+      }
 
 userStats = {:all => User.active.all.count}
 
