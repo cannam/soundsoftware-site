2bb6400f201296bdff54ee5399c1bbb9708e68d2
Chris Cannam <c.cannam@qmul.ac.uk>
2015-05-19 16:14:34 +0100

    Add script to print out a committer authormap file for a project's repo

---
+++ extra/soundsoftware/get-repo-authormap.rb
@@ -0,0 +1,18 @@
+
+# Print out an authormap file for hg-to-git conversion using
+# hg-fast-export
+#
+# Invoke with the project identifier as argument, e.g.
+#
+# ./script/rails runner -e production extra/soundsoftware/get-repo-authormap.rb soundsoftware-site
+
+proj_ident = ARGV[1]
+
+proj = Project.find_by_identifier(proj_ident)
+repo = Repository.where(:project_id => proj.id).first
+csets = Changeset.where(:repository_id => repo.id)
+committers = csets.map do |c| c.committer end.sort.uniq
+committers.each do |c|
+  u = repo.find_committer_user c
+  print "#{c}=#{u.name} <#{u.mail}>\n" unless u.nil?
+end
