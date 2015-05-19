3f504c3840b311e14c98e921e42932edb5ca3e09
Chris Cannam <c.cannam@qmul.ac.uk>
2015-05-19 17:33:38 +0100

    Authormap generator works, but is far too slow to run through runner

--- extra/soundsoftware/get-repo-authormap.rb
+++ extra/soundsoftware/get-repo-authormap.rb
@@ -6,13 +6,14 @@
 #
 # ./script/rails runner -e production extra/soundsoftware/get-repo-authormap.rb soundsoftware-site
 
-proj_ident = ARGV[1]
-
+proj_ident = ARGV.last
 proj = Project.find_by_identifier(proj_ident)
 repo = Repository.where(:project_id => proj.id).first
 csets = Changeset.where(:repository_id => repo.id)
 committers = csets.map do |c| c.committer end.sort.uniq
 committers.each do |c|
-  u = repo.find_committer_user c
-  print "#{c}=#{u.name} <#{u.mail}>\n" unless u.nil?
+  if not c =~ /[^<]+<.*@.*>/ then
+    u = repo.find_committer_user c
+    print "#{c}=#{u.name} <#{u.mail}>\n" unless u.nil?
+  end
 end
