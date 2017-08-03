e1b1d718c0bba35cff4648da5690be274de6001a
Chris Cannam <c.cannam@qmul.ac.uk>
2017-08-03 20:51:41 +0100

    Accept password from user

--- extra/soundsoftware/dockertest/start.sh
+++ extra/soundsoftware/dockertest/start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+dbpwd="$1"
+if [ -z "$dbpwd" ]; then
+    echo "Usage: $0 <database-password>" 1>&2
+    exit 2
+fi
+
 set -eu
 
 dockerdir=./extra/soundsoftware/dockertest
@@ -7,6 +13,12 @@ if [ ! -d "$dockerdir" ]; then
     echo "Run this script from the root of a working copy of soundsoftware-site"
     exit 2
 fi
+
+for f in database.yml code.conf ; do
+    cat "$dockerdir/$f" |
+        sed 's/INSERT_POSTGRES_PASSWORD_HERE/'"$dbpwd"'/g' > \
+            "$dockerdir/$f.interpolated"
+done
 
 dockertag="cannam/soundsoftware-site"
 
