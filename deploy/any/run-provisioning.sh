610256ddb7a172acbf9dacf9c9898470fb3460e2
Chris Cannam <c.cannam@qmul.ac.uk>
2017-08-24 14:46:03 +0100

    Directory fix

--- deploy/any/run-provisioning.sh
+++ deploy/any/run-provisioning.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 mydir=$(dirname "$0")
+case "$mydir" in
+    /*) ;;
+    *) mydir=$(echo "$(pwd)/$mydir" | sed 's,/\./,/,g')
+esac
 
 if [ "$mydir" != "/code-to-deploy/deploy/any" ]; then
     echo "ERROR: Expected repository to be at /code-to-deploy prior to provisioning"
+    echo "       (My directory is $mydir, expected /code-to-deploy/deploy/any)"
     exit 2
 fi
 
